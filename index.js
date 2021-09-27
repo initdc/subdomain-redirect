fa2861050d6a7d686115d241834a6f5f5c542e9e
initdc <initd@outlook.com>
2021-09-27 14:50:47 +0800

    [1.1.0] split better than regex

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const code = 302
 
 async function handleRequest(request) {
   const host = request.headers.get('host')
-  // console.log(host)
+  console.log(host)
 
-  const regex= /^\w+/
-  const found = host.match(regex)[0]
-  // console.log(found)
+  const found = host.split(".")[0]
+  
+  console.log(found)
 
   const value = await SUB.get(found)
   if (value === null) {
@@ -17,6 +17,10 @@ async function handleRequest(request) {
   return Response.redirect(value, code)
 }
 
-addEventListener('fetch', async event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event) => {
+    event.respondWith(
+        handleRequest(event.request).catch(
+            (err) => new Response(err.stack, { status: 500 })
+        )
+    );
+});
